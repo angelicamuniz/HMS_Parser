63776e4c4593661329137bb74ef422a1d17391cd
Angelica Muniz <angelica.muniz@neurobots.com.br>
2022-06-08 09:53:34 -0300

    Criação de entrada para o parser representando a máquina de estados do semáforo e do exemplo do Samek. Pasta com cada máquina foi criada para facilitar a organização.

---
+++ HSM-Samek/guard_and_actions.h
@@ -0,0 +1,21 @@
+
+int guard1()
+{
+    /* Desenvolva aqui sua funcao.*/
+    return 1;
+}
+
+
+int action1()
+{
+    /* Desenvolva aqui sua funcao.*/
+    return 1;
+}
+
+
+int action0()
+{
+    /* Desenvolva aqui sua funcao.*/
+    return 1;
+}
+
